53e8d5cfc8351fc3d81c489f10b648e04d970ea7
Lukas Vögtle <5764745+voegtlel@users.noreply.github.com>
2024-12-07 20:52:24 +0100

    Refactor DB, remove timestamps

---
+++ src/fjaelllada/db/text_db.py
@@ -0,0 +1,84 @@
+from abc import abstractmethod
+from contextlib import contextmanager
+from filelock import FileLock
+from pathlib import Path
+from typing import Generator, List, Tuple
+
+from fjaelllada.db import Db
+
+
+class TextDatabase(Db):
+    # *data, (offset_entry, size_entry)
+    _entries: List[tuple]
+    _path: Path
+    _lockfile: Path
+
+    def __init__(self, path: str):
+        self._path = Path(path)
+        self._lockfile = self._path.with_name(self._path.name + ".lock")
+        self._read_file()
+
+    @contextmanager
+    def _lock(self):
+        lock = FileLock(self._lockfile)
+        with lock:
+            self._check_update()
+            yield
+    
+    @abstractmethod
+    def _reset(self): ...
+
+    @abstractmethod
+    def _add_entry(self, fields: Tuple[str]): ...
+    
+    def _read_file(self):
+        self._entries = []
+        if self._path.is_file():
+            self._last_changed = self._path.stat().st_mtime_ns
+            with open(self._path, 'rb') as rf:
+                for line in rf:
+                    if not line.strip():
+                        continue
+                    fields = line.strip().rsplit(b";")
+                    entry = (*fields, (rf.tell() - len(line), len(line.rstrip())))
+                    self._entries.append(entry)
+                    self._add_entry(entry)
+
+    def _check_update(self):
+        if self._path.stat().st_mtime_ns > self._last_changed:
+            self._read_file()
+
+    def is_empty(self) -> bool:
+        with self._lock():
+            return len(self._entries) == 0
+
+    def _register(self, fields: Tuple[str, ...]) -> None:
+        with self._lock():
+            entry_raw = (";".join(fields) + "\n").encode()
+            with open(self._path, 'ab') as wf:    
+                entry_offset = wf.tell()
+                wf.write(entry_raw)
+                wf.flush()
+            self._entries.append((*fields, (entry_offset, len(entry_raw) - 1)))
+            self._add_entry(fields)
+
+    @abstractmethod
+    def _remove(self, index: int, fields: tuple): ...
+
+    def remove(self, index: int):
+        # This must be fetched before updating
+        *fields, (entry_offset, entry_size) = self._entries[index]
+        with self._lock():
+            print(f"Remove {index}")
+            del self._entries[index]
+            with open(self._path, 'rb+') as wf:
+                wf.seek(entry_offset)
+                # Blank out the entry
+                wf.write(b" " * entry_size)
+                wf.flush()
+            self._remove(index, fields)
+
+    def _get_all(self) -> Generator[tuple, None, None]:
+        with self._lock():
+            for *data, _offs in self._entries:
+                yield data
--- src/fjaelllada/db/totp_db.py
+++ src/fjaelllada/db/totp_db.py
@@ -1,95 +1,53 @@
-from datetime import datetime, timezone
-import os.path
+from pathlib import Path
 from typing import Generator, Tuple, cast, Optional, List
 
 import pyotp
 
-from fjaelllada.db import Db
 
-issuer = "jdav-locker-v1"
+from fjaelllada.db.text_db import TextDatabase
+from fjaelllada.env import TOTP_ISSUER
+
 valid_window = 1
 
 
-def now_timestamp() -> str:
-    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-
-
-def timestamp_to_localtz(timestamp: str) -> datetime:
-    timestamp_dt = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-    timestamp_dt = timestamp_dt.replace(tzinfo=timezone.utc)
-    return datetime.fromtimestamp(timestamp_dt.timestamp())
-
-
-class TotpDatabase(Db):
-    _entries: List[pyotp.TOTP]
-    # uri, last_login, (offset_last_login, size_last_login), (offset_entry, size_entry)
-    _orig_entries: List[Tuple[bytes, bytes, Tuple[int, int], Tuple[int, int]]]
+class TotpDatabase(TextDatabase):
+    _totp_entries: List[pyotp.TOTP]
+    _path: Path
+    _lockfile: Path
 
     def __init__(self, path: str):
-        self._f = open(path, 'rb+')
-        self._orig_entries = []
-        if os.path.isfile(path):
-            for line in self._f:
-                if not line.strip():
-                    continue
-                uri, last_login = line.strip().rsplit(b";", 1)
-                date_offset = self._f.tell() - len(line) + line.rindex(b';') + 1
-                self._orig_entries.append((uri, last_login, (date_offset, len(last_login)), (self._f.tell() - len(line), len(line.rstrip()))))
-
-            self._entries = [
-                cast(pyotp.TOTP, pyotp.parse_uri(entry.decode()))
-                for entry, *_ in self._orig_entries
-            ]
-        else:
-            self._entries = []
-        print("Initial totp db: ", list(self.get_all()))
-
-    def is_empty(self) -> bool:
-        return len(self._entries) == 0
+        super().__init__(path, 1)
+    
+    def _reset(self):
+        self._totp_entries = []
+    
+    def _add_entry(self, fields: Tuple[str]):
+        self._totp_entries.append(cast(pyotp.TOTP, pyotp.parse_uri(fields[0].decode())))
 
     def register(self, provisioning_uri: str, verification_code: Optional[str] = None) -> bool:
         totp = cast(pyotp.TOTP, pyotp.parse_uri(provisioning_uri))
         if verification_code is not None:
             if not totp.verify(verification_code, valid_window=valid_window):
                 return False
-        last_login = now_timestamp()
-        entry_offset = self._f.tell()
-        entry_raw = (provisioning_uri + ";" + last_login + "\n").encode()
-        self._f.write(entry_raw)
-        self._f.flush()
-        last_login_offset = self._f.tell() - len(last_login) - 1
-        self._entries.append(totp)
-        self._orig_entries.append((provisioning_uri.encode(), last_login.encode(), (last_login_offset, len(last_login)), (entry_offset, len(entry_raw) - 1)))
+        self._register((provisioning_uri,))
         return True
 
     def verify(self, code: str):
-        for idx, entry in enumerate(self._entries):
-            if entry.verify(code, valid_window=valid_window):
-                uri, timestamp, (timestamp_offset, timestamp_size), entry_offsets = self._orig_entries[idx]
-                timestamp = now_timestamp().encode()
-                self._orig_entries[idx] = (uri, timestamp, (timestamp_offset, timestamp_size), entry_offsets)
-                if timestamp_size == len(timestamp):
-                    self._f.seek(timestamp_offset)
-                    self._f.write(timestamp)
-                    self._f.seek(0, 2)
-                return True
-        #return False or code == "000000"
-        return False
+        with self._lock():
+            for entry in self._totp_entries:
+                if entry.verify(code, valid_window=valid_window):
+                    return True
+            #return False or code == "000000"
+            return False
 
-    def remove(self, index: int):
-        entry_offset, entry_size = self._orig_entries[index][3]
-        del self._entries[index]
-        del self._orig_entries[index]
-        self._f.seek(entry_offset)
-        # Blank out the entry
-        self._f.write(b" " * entry_size)
-        self._f.flush()
-        self._f.seek(0, 2)
+    def _remove(self, index: int, fields: tuple):
+        del self._totp_entries[index]
 
-    def get_all(self) -> Generator[Tuple[str, datetime], None, None]:
-        for totp, (_, last_login, *_) in zip(self._entries, self._orig_entries):
-            yield totp.name, timestamp_to_localtz(last_login.decode())
+    def get_all(self) -> Generator[str, None, None]:
+        with self._lock():
+            for totp, *_ in self._totp_entries:
+                yield totp.name
 
     @staticmethod
     def new_uri(username: str) -> str:
-        return pyotp.TOTP(pyotp.random_base32(), name=username, issuer=issuer).provisioning_uri()
+        return pyotp.TOTP(pyotp.random_base32(), name=username, issuer=TOTP_ISSUER).provisioning_uri()
