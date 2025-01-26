4772bf9c206e5b96cc33ba3b70a61e85e5136813
Andreas Philipp <agf-ghg@t-online.de>
2025-01-27 00:01:24 +0100

    hotfix: adapt pin numbers to current pinout

--- software/fjaelllada/env.py
+++ software/fjaelllada/env.py
@@ -11,5 +11,5 @@ SCREEN_RESOLUTION_HEIGHT = int(os.environ.get("SCREEN_RESOLUTION_HEIGHT", "320")
 
 TOTP_ISSUER = os.environ.get("TOTP_ISSUER", "jdav-locker-v1")
 
-INPUT_PIN = int(os.environ.get("INPUT_PIN", "20"))
-OUTPUT_PIN = int(os.environ.get("OUTPUT_PIN", "21"))
+INPUT_PIN = int(os.environ.get("INPUT_PIN", "21"))
+OUTPUT_PIN = int(os.environ.get("OUTPUT_PIN", "20"))
